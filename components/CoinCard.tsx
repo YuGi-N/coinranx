5c463e0382837368908eb766ef442ff1d4a2e1f7
Yoggi-n <yogeswaran.nn@outlook.com>
2022-09-18 15:48:45 +0530

    Added coin card component to display coin data

--- components/CoinCard.tsx
+++ components/CoinCard.tsx
@@ -12,12 +12,12 @@ const CoinCard = ({ data }:CoinCardProps) => {
 
     return (
         <div className='w-full md:max-w-4xl bg-neutral-900 h-auto md:h-80 md:rounded-md flex flex-col md:flex-row'>
-            <div className='flex flex-col justify-center items-center w-full md:w-64 gap-5 border border-neutral-700 py-6'>
+            <div className='flex flex-col justify-center items-center w-full md:w-64 gap-5 border-b md:border-r border-neutral-700 py-6'>
                 <picture><img className='w-56 h-56' src={data.image.large} alt={`${data.name}_logo`} /></picture>
-                <h1 className=''>{data.name}</h1>
+                <h1 className='text-xl'>{data.name}</h1>
             </div>
             <div className='flex flex-col flex-grow'>
-                <div className='w-full flex flex-row items-center justify-center h-32 gap-8 flex-shrink text-xl'>
+                <div className='w-full flex flex-row items-center justify-center h-32 gap-8 flex-shrink text-xl border-b border-neutral-700'>
                     <p className='inline-block'>{`${price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}$`}</p>
                     <div className='flex flex-row items-center gap-1'>
                         {
@@ -34,10 +34,10 @@ const CoinCard = ({ data }:CoinCardProps) => {
                     </div>
                 </div>
                 <div className='flex-grow grid grid-cols-2 items-center text-center w-full h-60 md:h-auto'>
-                    <p className='h-full w-full flex items-center justify-center text-xl'>{`All Time High: ${data.market_data.ath.usd.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}$`}</p>
-                    <p className='h-full w-full flex items-center justify-center text-xl'>{`All Time Low: ${data.market_data.atl.usd.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}$`}</p>
-                    <p className='h-full w-full flex items-center justify-center text-xl'>{`Market cap rank: ${data.market_cap_rank}`}</p>
-                    <p className='h-full w-full flex items-center justify-center text-xl'>{`Market cap: ${data.market_data.market_cap.usd.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}$`}</p>
+                    <p className='h-full w-full flex items-center justify-center text-lg'>{`All Time High: ${data.market_data.ath.usd.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}$`}</p>
+                    <p className='h-full w-full flex items-center justify-center text-lg'>{`All Time Low: ${data.market_data.atl.usd.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}$`}</p>
+                    <p className='h-full w-full flex items-center justify-center text-lg'>{`Market cap rank: ${data.market_cap_rank}`}</p>
+                    <p className='h-full w-full flex items-center justify-center text-lg'>{`Market cap: ${data.market_data.market_cap.usd.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}$`}</p>
                 </div>
             </div>
         </div>
