acb852355b6e9b1d1e6e58fefed96b13b37f572f
Yoggi-n <yogeswaran.nn@outlook.com>
2022-09-18 19:29:31 +0530

    Removed border from card

--- components/CoinCard.tsx
+++ components/CoinCard.tsx
@@ -12,7 +12,7 @@ const CoinCard = ({ data }:CoinCardProps) => {
 
     return (
         <div className='w-full md:max-w-4xl bg-neutral-900 h-auto md:h-80 md:rounded-md flex flex-col md:flex-row'>
-            <div className='flex flex-col justify-center items-center w-full md:w-64 gap-5 border-b md:border-r border-neutral-700 py-6'>
+            <div className='flex flex-col justify-center items-center w-full md:w-64 gap-5 border-b md:border-r md:border-b-0 border-neutral-700 py-6'>
                 <picture><img className='w-56 h-56' src={data.image.large} alt={`${data.name}_logo`} /></picture>
                 <h1 className='text-xl'>{data.name}</h1>
             </div>
