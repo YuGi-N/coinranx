168663d4a60eefe04f9c0a88fd44b8532b977297
Yoggi-n <yogeswaran.nn@outlook.com>
2022-09-18 12:21:35 +0530

    Create empty favourites in localstorage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  	return <Component {...pageProps} />
+
+	useEffect(() => {
+		const storedFavourites = localStorage.getItem('favourites');
+		if(!storedFavourites) {
+			localStorage.setItem('favourites', JSON.stringify([]));
+		} 
+	}, []);
+
+  	return (
+		<div className='bg-neutral-800 w-screen min-h-screen text-white'>
+			<Component {...pageProps} />
+		</div>
+	)
 }
 
 export default MyApp;
