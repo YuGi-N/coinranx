17d63f357b413fba55fa19f696cdbdcc71aaa07e
Yoggi-n <yogeswaran.nn@outlook.com>
2022-09-18 15:52:31 +0530

    Display coin chart

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -2,6 +2,7 @@ import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import CoinCard from '../components/CoinCard';
+import CoinChart from '../components/CoinChart';
 import Header from '../components/Header';
 import fetcher from '../util/fetcher';
 
@@ -12,14 +13,17 @@ const CoinData: NextPage = () => {
     const { data: coinData, error: coinError } = useSWR(id ? `https://api.coingecko.com/api/v3/coins/${id}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false` : null, fetcher);
     const { data: coinHistoryData, error: coinHistoryError } = useSWR(id ? `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=7&interval=daily` : null, fetcher);
 
-    console.log(coinHistoryData);
-
     return (
         <div>
             <Header />
             <div className='w-full flex flex-col items-center md:mt-8 font-inter'>
                 <>
                     { coinData && <CoinCard data={coinData} /> }
+                </>
+            </div>
+            <div className='w-full flex flex-col items-center md:mt-8 font-inter'>
+                <>
+                    { coinHistoryData && coinData && <CoinChart data={coinHistoryData} coinData={coinData} />}
                 </>
             </div>
         </div>
